4c6ef6f3fd97e8243a30c42e0692ebd4ed7f0ebb
suki-sahota <46114711+suki-sahota@users.noreply.github.com>
2019-02-19 20:56:39 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    language: 'Spanish',
+    hoursStudied: 274,
+    firstName: '',
+    lastName: '',
+    email: '',
+    ticketQuantity: 1,
+    ticketType: 'general',
+    referrals: [],
+    specialRequests: '',
+    purchaseAgreementSigned: false
+  }
+});
